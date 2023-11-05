967e4031753050d7101f7565cee817b72c4cb60f
juliasol1 <71459971+juliasol1@users.noreply.github.com>
2023-11-05 19:12:35 +0000

    db creation and init

---
+++ gen-product-names.py
@@ -0,0 +1,13 @@
+from faker import Faker
+
+import faker_commerce
+
+fake = Faker()
+fake.add_provider(faker_commerce.Provider)
+no_items=3000
+
+with open('db_init_out.sql',"a+") as g:
+    for i in range(no_items):
+        product_name = fake.ecommerce_name()
+        line = ''.join(product_name)
+        g.write(line)
